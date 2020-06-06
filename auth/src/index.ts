d681a2a276c9704dbbfe68da0853613feb3b2f95
Florian GOTO <you@example.com>
2020-06-07 01:59:47 +0200

    add typed mongoose usermodel

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import {
   currentUserRouter,
   signupRouter,
@@ -25,8 +26,22 @@ app.all(
 
 app.use(expressCustomErrorHandler);
 
-app.listen(PORT, () => {
+const start = async () => {
   console.log(`updatesd ${new Date().toISOString()}`);
+  try {
+    await mongoose.connect("mongodb://auth-mongo-clusterip-svc:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Auth service connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
 
-  console.log(`Auth service listening on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Auth service listening on port ${PORT}`);
+  });
+};
+
+start();
---
+++ auth/src/models/user.model.ts
@@ -0,0 +1,35 @@
+import { Schema, model, Model, Document } from "mongoose";
+
+// for type checking schema attributes
+interface UserAttributes {
+  email: string;
+  password: string;
+}
+
+// describe extended mongoose model
+interface UserModel extends Model<UserDocument> {
+  build(attr: UserAttributes): UserDocument;
+}
+
+// describes a User document
+interface UserDocument extends Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.statics.build = (attr: UserAttributes) => {
+  return new User();
+};
+
+export const User = model<UserDocument, UserModel>("User", userSchema);
